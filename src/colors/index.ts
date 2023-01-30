25d4b00f722cff98ad49e3658d6a0a9fdeb8e1d2
Matt Brannon <mattbrannon.dev@gmail.com>
2023-01-29 21:30:48 -0600

    export all color conversion functions

---
+++ src/colors/index.ts
@@ -0,0 +1,17 @@
+import { cmyk } from './cmyk';
+import { hex } from './hex';
+import { hsl } from './hsl';
+import { hsv } from './hsv';
+import { hwb } from './hwb';
+import { lch } from './lch';
+import { rgb } from './rgb';
+
+export const colors = {
+  cmyk,
+  hex,
+  hsl,
+  hsv,
+  hwb,
+  lch,
+  rgb,
+};
