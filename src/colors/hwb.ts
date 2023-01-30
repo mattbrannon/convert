ca66926411b39d78ed22a0210b86ac702799c892
Matt Brannon <mattbrannon.dev@gmail.com>
2023-01-30 04:00:55 -0600

    remove comment

--- src/colors/hwb.ts
+++ src/colors/hwb.ts
@@ -18,8 +18,6 @@ function hwbToRgb(...hwb: number[]): number[] {
     const i = h | 0;
     const f = i & 1 ? 1 - (h - i) : h - i;
 
-    // f = h - i;
-    // if (i & 1) f = 1 - f;
     n = w + f * (v - w);
     v = (v * 255) | 0;
     n = (n * 255) | 0;
