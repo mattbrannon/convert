dbd94601b07262f8a2a762c2379a65ab6a3fbc26
Matt Brannon <mattbrannon.dev@gmail.com>
2023-02-03 22:15:58 -0600

    update build and exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,20 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
-import pkg from './package.json';
 
 export default [
   {
     plugins: [ commonjs(), resolve(), terser() ],
-    input: 'dist/index.js',
+    input: 'compiled/index.js',
     output: [
       {
-        dir: 'dist',
+        dir: 'dist/esm',
         format: 'esm',
         preserveModules: true,
       },
       {
         exports: 'named',
-        file: pkg.exports.require,
+        file: 'dist/cjs/index.js',
         format: 'cjs',
       },
     ],
