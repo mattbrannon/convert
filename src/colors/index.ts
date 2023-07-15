613772d0698ec70dbc7f7747f01f0998403772f9
Matt Brannon <mattbrannon.dev@gmail.com>
2023-07-14 23:55:15 -0500

    clean up exports

--- src/colors/index.ts
+++ src/colors/index.ts
@@ -1,11 +1,3 @@
-import { cmyk } from './cmyk';
-import { hex } from './hex';
-import { hsl } from './hsl';
-import { hsv } from './hsv';
-import { hwb } from './hwb';
-import { lch } from './lch';
-import { rgb } from './rgb';
-
 export { cmyk } from './cmyk';
 export { hex } from './hex';
 export { hsl } from './hsl';
@@ -13,13 +5,3 @@ export { hsv } from './hsv';
 export { hwb } from './hwb';
 export { lch } from './lch';
 export { rgb } from './rgb';
-
-export const colors = {
-  cmyk,
-  hex,
-  hsl,
-  hsv,
-  hwb,
-  lch,
-  rgb,
-};
