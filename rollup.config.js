fce9ba8ab50794aad0854ebf199cca8e44bb1ee8
Matt Brannon <mattbrannon.dev@gmail.com>
2023-07-15 22:56:49 -0500

    update build process

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,25 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 
+import pkg from './package.json';
+
 export default [
   {
     plugins: [ commonjs(), resolve(), terser() ],
     input: 'compiled/index.js',
     output: [
       {
-        dir: 'dist/esm',
         format: 'esm',
-        preserveModules: true,
+        file: pkg.exports.import,
       },
       {
-        exports: 'named',
-        file: 'dist/cjs/index.js',
         format: 'cjs',
+        file: pkg.exports.require,
+      },
+      {
+        format: 'umd',
+        name: 'convert',
+        file: pkg.exports.default,
       },
     ],
   },
