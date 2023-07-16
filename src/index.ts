b5cdb65735fda3143cae8b19614bc3b57e95be13
Matt Brannon <mattbrannon.dev@gmail.com>
2023-07-15 22:57:38 -0500

    include default export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-export { cmyk, hex, hsl, hsv, hwb, lch, rgb } from './colors';
-export { radian, gradian, degree, turn } from './angles';
+import { cmyk, hex, hsl, hsv, hwb, lch, rgb } from './colors';
+import { radian, gradian, degree, turn } from './angles';
+
+export default {
+  cmyk,
+  hex,
+  hsl,
+  hsv,
+  hwb,
+  lch,
+  rgb,
+  radian,
+  gradian,
+  degree,
+  turn,
+};
+
+export { cmyk, hex, hsl, hsv, hwb, lch, rgb, radian, gradian, degree, turn };
