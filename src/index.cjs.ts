6268f60dba48e7fc4129197a598ce7e2011f650e
Matt Brannon <mattbrannon.dev@gmail.com>
2023-07-23 05:41:04 -0500

    include separate esm and cjs inputs to build process

---
+++ src/index.cjs.ts
@@ -0,0 +1,4 @@
+import { cmyk, hex, hsl, hsv, hwb, lch, rgb } from './colors';
+import { radian, gradian, degree, turn } from './angles';
+
+export { cmyk, hex, hsl, hsv, hwb, lch, rgb, radian, gradian, degree, turn };
