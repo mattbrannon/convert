560282c8266fdfea4a4f1a8c408683ec5f387b8c
Matt Brannon <mattbrannon.dev@gmail.com>
2023-08-10 13:37:50 -0500

    removed

--- src/index.cjs.ts
+++
@@ -1,4 +0,0 @@
-import { cmyk, hex, hsl, hsv, hwb, lch, rgb } from './colors';
-import { radian, gradian, degree, turn } from './angles';
-
-export { cmyk, hex, hsl, hsv, hwb, lch, rgb, radian, gradian, degree, turn };
